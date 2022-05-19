5aa53cdadd517c780753fb9247e4f262cc4c0152
David Bass <davidbass256@gmail.com>
2022-05-19 13:24:54 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -10,7 +10,7 @@ data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 
 print(1)
 
-fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=[0],
+fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["0"],
                                      projections=[5])
 
 print (2)
