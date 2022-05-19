0cb2c920fe4d02cfe53bd7f7cdc5b5d989b61dea
David Bass <davidbass256@gmail.com>
2022-05-19 11:21:33 -0400

    Delete R code directory

--- R code/5-16 combined model pi values.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-
-setwd("C:\\Users\\David\\Desktop\\Bergland\\R")
-data <- read.table("5-16 combined_models.pi", header=TRUE)
-plot_line <- ggplot(data, aes(x=BIN_END, y=(PI), color=MODEL)) + geom_line() + geom_point()
-plot_line
