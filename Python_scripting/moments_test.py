d552b84bab1aacab0251bf6a2ec86e7fc352ef78
David Bass <davidbass256@gmail.com>
2022-05-19 13:03:22 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -1,9 +1,12 @@
 import moments
 print(-1)
-input_vcf = "/scratch/djb3ve/data/3islands_4mig.vcf"
+
+input_vcf = "/scratch/djb3ve/data/small_vcf.vcf"
+input_popinfo = "/scratch/djb3ve/data/small_vcf_popinfo.txt"
 
 print(0)
-data_dict = moments.Misc.make_data_dict_vcf(input_vcf)
+
+data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 
 print(1)
 
