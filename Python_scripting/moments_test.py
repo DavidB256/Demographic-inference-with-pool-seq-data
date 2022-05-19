6137545c58a818acc034601d0766060a36ebdae7
David Bass <davidbass256@gmail.com>
2022-05-19 12:04:48 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -2,5 +2,11 @@ import moments
 
 input_vcf = "/scratch/djb3ve/data/3islands_4mig.vcf"
 
+print(0)
 data_dict = moments.Misc.make_data_dict_vcf(input_vcf)
+
+print(1)
+
 fs = moments.Spectrum.from_data_dict(data_dict)
+
+print (2)
