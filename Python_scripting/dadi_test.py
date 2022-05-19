a1415c0206e0004bff97396259615f6b69f29559
David Bass <davidbass256@gmail.com>
2022-05-19 15:49:24 -0400

    e

--- Python_scripting/dadi_test.py
+++ Python_scripting/dadi_test.py
@@ -11,7 +11,7 @@ data_dict = dadi.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 
 print(1)
 
-fs = dadi.Spectrum.from_data_dict(data_dict, pop_ids=["0"],
+fs = dadi.Spectrum.from_data_dict(data_dict, pop_ids=["pop0"],
                                      projections=[5], polarized=False)
 
 print (2)
