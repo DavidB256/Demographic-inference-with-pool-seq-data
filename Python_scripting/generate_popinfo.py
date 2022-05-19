5dfbd449b02aaf658e6cdcaae524f6bd19bd92ac
David Bass <davidbass256@gmail.com>
2022-05-19 16:09:05 -0400

    e

---
+++ Python_scripting/generate_popinfo.py
@@ -0,0 +1,14 @@
+import sys
+
+# Handle command line arguments
+if len(sys.argv) < 2:
+    print("Error: One command line argument is required.")
+    sys.exit()
+sample_size = sys.argv[1]
+
+output_file = "/scratch/djb3ve/data/popinfo.txt"
+
+with open(output_file, "w") as out:
+    for i in range(sample_size):
+        sample_name = "tsk_" + str(i)
+        out.write("%s\tpop0" % (sample_name))
