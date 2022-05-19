9a2099dc2afd4fdf6d3f623b3997dfc077df5842
David Bass <davidbass256@gmail.com>
2022-05-19 15:44:31 -0400

    e

---
+++ Python_scripting/dadi_test.py
@@ -0,0 +1,17 @@
+import dadi
+
+print(-1)
+
+input_vcf = "/scratch/djb3ve/data/small_vcf.vcf"
+input_popinfo = "/scratch/djb3ve/data/small_vcf_popinfo.txt"
+
+print(0)
+
+data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
+
+print(1)
+
+fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["0"],
+                                     projections=[5], polarized=False)
+
+print (2)
