3612b570072f38ca2b78ffcf6afe5a23da3aac58
David Bass <davidbass256@gmail.com>
2022-05-20 14:13:23 -0400

    e

--- Python_scripting/write_popinfo_file_for_2island_model.py
+++ Python_scripting/write_popinfo_file_for_2island_model.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 2:
 sample_size = int(sys.argv[1])
 
 with open(out_dir + "popinfo_file_for_2island_model_%dn.txt"
-          % (sample_size)) as out:
+          % (sample_size), "w") as out:
     for i in range(sample_size):
         out.write("tsk_%d\tpop0\n" % (i))
     for i in range(sample_size):
