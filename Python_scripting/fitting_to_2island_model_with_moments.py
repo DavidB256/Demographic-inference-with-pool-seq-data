87112be64a319f4b527afc89211a4091b9e09a1b
David Bass <davidbass256@gmail.com>
2022-05-23 22:50:59 -0400

    e

---
+++ Python_scripting/fitting_to_2island_model_with_moments.py
@@ -0,0 +1,41 @@
+import moments
+import dadi
+
+# Import VCF file from prior msprime simulation and popinfo file
+vcf = "/scratch/djb3ve/data/small_vcf.vcf"
+popinfo = "/scratch/djb3ve/data/popinfo.txt"
+output = "/scratch/djb3ve/"
+pts = 100
+ns = [10, 10]
+
+data_dict = dadi.make_data_dict_vcf(vcf, popinfo)
+print("VCF imported.")
+fs = dadi.Spectrum.from_data_dict(data_dict, pop_ids=["pop0", "pop1"],
+                                     projections=[5], polarized=False)
+print("VCF converted to SFS.")
+
+def two_island_admixture(params, ns):
+    nu1, nu2, T, m = params
+    xx = moments.default_grid(pts)
+
+    phi = moments.PhiManip.phi_1D(xx)
+    phi = moments.PhiManip.phi_1D_to_2D(xx, phi)
+    phi = moments.Integration.two_pops(phi, xx, T, nu1, nu2, m12=m, m21=m)
+    fs = moments.Spectrum.from_phi(phi, ns, (xx, xx))
+
+    return fs
+
+print("Model function defined.")
+
+mig_rates = [i * 0.01 for i in range(25)]
+out_f = open(output, "w")
+
+for mig_rate in mig_rates:
+    params = [100, 100, 0, mig_rate]
+    popt = moments.Inference.optimate_log(params, fs, two_island_admixture, pts)
+    model = two_island_admixture(popt, ns)
+
+    ll_model = moments.Inference.ll_multinom(model, fs)
+    print("%f\t%f" % (mig_rate, ll_model))
+
+out_f.close()
