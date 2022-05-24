13d11ef9cac69fd05907b4a4a216d779d2d83d57
David Bass <davidbass256@gmail.com>
2022-05-24 00:36:09 -0400

    e

--- Python_scripting/fitting_to_2island_model_with_moments.py
+++ Python_scripting/fitting_to_2island_model_with_moments.py
@@ -32,8 +32,8 @@ def two_island_admixture(params, ns):
 print("Model function defined.")
 
 mig_rates = [i * 0.01 for i in range(25)]
-lower_bound = [100, 100, 0.01, 0.01]
-upper_bound = [100, 100, 2, 1]
+lower_bound = [100, 100, 1, 0.01]
+upper_bound = [100, 100, 1, 1]
 out_f = open(output, "w")
 
 for mig_rate in mig_rates:
