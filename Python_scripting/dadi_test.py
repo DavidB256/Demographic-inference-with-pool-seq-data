ce81c91e1a58a8cdf42517b4a85f885d8bc94e7a
David Bass <davidbass256@gmail.com>
2022-05-27 16:07:18 -0400

    e

--- Python_scripting/dadi_test.py
+++ Python_scripting/dadi_test.py
@@ -7,7 +7,7 @@ input_popinfo = "/scratch/djb3ve/data/popinfo_for_small_vcf.txt"
 # Load VCF and popinfo file into "data_dict" in order to make SFS "fs"
 data_dict = dadi.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 fs = dadi.Spectrum.from_data_dict(data_dict, pop_ids=["pop0"],
-                                     projections=[5], polarized=False)
+                                     projections=[5], polarized=True)
 
 # Calculate and print pi
 pi = fs.pi()
