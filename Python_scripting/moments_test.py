da7c722916b41f4e3f1b47dde4f559268ee4dd2a
David Bass <davidbass256@gmail.com>
2022-05-30 18:39:25 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -7,8 +7,15 @@ input_popinfo = "/scratch/djb3ve/data/popinfo_for_small_vcf.txt"
 # Load VCF and popinfo file into "data_dict" in order to make SFS "fs"
 data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0"],
-                                     projections=[5], polarized=False)
+                                     projections=[10], polarized=False)
 
 # Calculate and print pi
 pi = fs.pi()
 print(fs)
+
+# Junk
+'''
+for i in data_dict:
+    print(i)
+    print(data_dict[i])
+'''
