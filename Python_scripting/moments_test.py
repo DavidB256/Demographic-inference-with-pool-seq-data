72903ad808ce32481ebd8a6482bccfb95c83596a
David Bass <davidbass256@gmail.com>
2022-06-01 14:01:09 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -7,6 +7,6 @@ input_popinfo = "/scratch/djb3ve/data/popinfo_3island_small_model.txt"
 # Load VCF and popinfo file into "data_dict" in order to make SFS "fs"
 data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
 fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0", "pop1", "pop2"],
-                                     projections=[4, 4, 4], polarized=True)
+                                     projections=[4, 4, 4], polarized=False)
 
 print(fs)
