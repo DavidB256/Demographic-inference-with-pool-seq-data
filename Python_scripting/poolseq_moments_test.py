7b8aebca1adcdf6287637ac25aaacba1a5adcf9f
David Bass <davidbass256@gmail.com>
2022-05-31 23:37:50 -0400

    e

--- Python_scripting/poolseq_moments_test.py
+++ Python_scripting/poolseq_moments_test.py
@@ -12,7 +12,7 @@ with open(input_fs, "r") as f_fs:
         line = f_fs.readline()
     f_fs.readline()
     while line:
-        values.append(int(line[-2]))
+        values.append(int(line[:-2]))
         line = f_fs.readline()
 
 arr = np.array(values).reshape(dims)
