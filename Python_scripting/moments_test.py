9a60d47f9697b48b5e6cba05924f744267b64495
David Bass <davidbass256@gmail.com>
2022-06-01 00:24:00 -0400

    e

--- Python_scripting/moments_test.py
+++ Python_scripting/moments_test.py
@@ -1,21 +1,12 @@
 import moments
 
 # Setup
-input_vcf = "/scratch/djb3ve/data/small_vcf.vcf"
-input_popinfo = "/scratch/djb3ve/data/popinfo_for_small_vcf.txt"
+input_vcf = "/scratch/djb3ve/data/3island_small_model.vcf"
+input_popinfo = "/scratch/djb3ve/data/popinfo_3island_small_model.txt"
 
 # Load VCF and popinfo file into "data_dict" in order to make SFS "fs"
 data_dict = moments.Misc.make_data_dict_vcf(input_vcf, input_popinfo)
-fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0"],
-                                     projections=[10], polarized=False)
+fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0", "pop1", "pop2"],
+                                     projections=[4, 4, 4], polarized=False)
 
-# Calculate and print pi
-pi = fs.pi()
 print(fs)
-
-# Junk
-'''
-for i in data_dict:
-    print(i)
-    print(data_dict[i])
-'''
