c12eb130273f29f915349626584be5d0aa95b12e
David Bass <davidbass256@gmail.com>
2022-06-03 12:18:18 -0400

    e

--- Python_scripting/moments_pipeline_test.py
+++ Python_scripting/moments_pipeline_test.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 # Import VCF file from prior msprime simulation and popinfo file
-vcf = "/scratch/djb3ve/data/alt_2island_1mig_model.vcf"
+vcf = "/scratch/djb3ve/data/alt_2island_2mig_model.vcf"
 popinfo = "/scratch/djb3ve/data/popinfo_file_for_2island_model_10n.txt"
 ns = [20, 20]
 prefix = sys.argv[1]
@@ -15,7 +15,7 @@ fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0", "pop1"],
 
 def two_island_admixture(params, ns):
     nu1, nu2, T = params
-    mig_rate = 0
+    mig_rate = 0.01
     return moments.Demographics2D.split_mig([nu1, nu2, T, mig_rate], ns,
                                             pop_ids=["pop0", "pop1"])
 
