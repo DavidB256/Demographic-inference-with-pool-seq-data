3478aff4399ff8e43e9bc567f0d6f097438735a5
David Bass <davidbass256@gmail.com>
2022-06-03 12:23:12 -0400

    e

--- Python_scripting/moments_pipeline_test.py
+++ Python_scripting/moments_pipeline_test.py
@@ -14,12 +14,11 @@ fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0", "pop1"],
                                      projections=ns, polarized=False)
 
 def two_island_admixture(params, ns):
-    nu1, nu2, T = params
-    mig_rate = 0.01
+    nu1, nu2, T, mig_rate = params
     return moments.Demographics2D.split_mig([nu1, nu2, T, mig_rate], ns,
                                             pop_ids=["pop0", "pop1"])
 
     return fs
 
 Optimize_Functions.Optimize_Routine(fs, prefix, "two_island_admixture",
-                                    two_island_admixture, 3, 3, fs_folded=True)
+                                    two_island_admixture, 4, 3, fs_folded=True)
