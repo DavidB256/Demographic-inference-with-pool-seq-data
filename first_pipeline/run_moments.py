dedb51e3fa82449fc2971ef2411b3d5482766f6f
David Bass <davidbass256@gmail.com>
2022-06-10 16:34:38 -0400

    e

--- first_pipeline/run_moments.py
+++ first_pipeline/run_moments.py
@@ -1,5 +1,6 @@
 import moments
 import numpy as np
+import sys
 
 def control_model(params, ns):
     # params = [nu, T]
@@ -12,8 +13,12 @@ def two_pop_split_model():
 instructions = "/scratch/djb3ve/data/pipeline_instructions.txt"
 
 # Everything below this is temporary.
+
+# Handle command line arguments
+iterations = int(sys.argv[1])
+
 vcf = "/scratch/djb3ve/data/first_models/control_demography_n10_seed1.vcf"
-popinfo = "/scratch/djb3ve/first_models/control_demography_n10_popinfo.txt"
+popinfo = "/scratch/djb3ve/data/first_models/control_demography_n10_popinfo.txt"
 ns = [10]
 
 data_dict = moments.Misc.make_data_dict_vcf(vcf, popinfo)
@@ -22,12 +27,20 @@ fs = moments.Spectrum.from_data_dict(data_dict, pop_ids=["pop0"], projections=ns
 lower_bound = [1e-3 for i in range(2)]
 upper_bound = [10 for i in range(2)]
 
+column_names_list = ["nu_init", "t_init", "nu_opt", "t_opt", "ll", "theta"]
+header = ""
+for name in column_names_list:
+    header += name + "\t"
+print(header)
 
-params = [np.random.uniform(lower_bound[j], upper_bound[j]) for j in range(2)]
-popt = moments.Inference.optimize_log(params, fs, control_model,
-                                      lower_bound=lower_bound,
-                                      upper_bound=upper_bound,
-                                      maxiter=100, verbose=1)
-model = control_model(popt, ns)
-ll_model = moments.Inference.ll_multinom(model, fs)
-theta = moments.Inference.optimal_sfs_scaling(model, fs)
+for i in range(iterations):
+    params = [np.random.uniform(lower_bound[j], upper_bound[j]) for j in range(2)]
+    popt = moments.Inference.optimize_log(params, fs, control_model,
+                                          lower_bound=lower_bound,
+                                          upper_bound=upper_bound,
+                                          maxiter=100, verbose=1)
+    model = control_model(popt, ns)
+    ll_model = moments.Inference.ll_multinom(model, fs)
+    theta = moments.Inference.optimal_sfs_scaling(model, fs)
+
+    print("%f\t" * len(column_names_list) % (params[0], params[1], popt[0], popt[1], ll_model, theta))
