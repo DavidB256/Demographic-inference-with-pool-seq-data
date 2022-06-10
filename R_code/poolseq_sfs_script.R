342629401424f3bcae10d5ebf58fac0e6be87bf9
David Bass <davidbass256@gmail.com>
2022-06-10 03:00:08 -0400

    e

--- R_code/poolseq_sfs_script.R
+++ R_code/poolseq_sfs_script.R
@@ -75,7 +75,7 @@ setwd("/scratch/djb3ve/data/first_models/")
 # Hands command line arguments
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) != 4) { stop("Error: Five command line arguments must be supplied.", call.=FALSE) }
-vcf_name <- args[1]
+vcf_name <- as.character(args[1])
 popinfo <- eval(parse(text=args[2])) 
 haploid_counts <- eval(parse(text=args[3]))
 poolseq_coverage <- as.numeric(args[4])
